59bd9cc9630a6acc4b2e04fc6634fa23effd56a4
jvo203 <chris.zapart@nao.ac.jp>
2024-05-18 13:30:37 +0900

    added 'binary_entropy' to the exports

--- src/nf.f90
+++ src/nf.f90
@@ -1,14 +1,14 @@
 module nf
-  !! User API: everything an application needs to reference directly
-  use nf_datasets_mnist, only: label_digits, load_mnist
-  use nf_layer, only: layer
-  use nf_layer_constructors, only: &
-    conv2d, dense, flatten, input, maxpool2d, reshape
-  use nf_loss, only: mse, quadratic
-  use nf_network, only: network
-  use nf_optimizers, only: sgd, rmsprop, adam, adagrad
-  use nf_activation, only: activation_function, elu, exponential,  &
-                           gaussian, linear, relu, leaky_relu,     &
-                           sigmoid, softmax, softplus, step, tanhf, &
-                           celu
+   !! User API: everything an application needs to reference directly
+   use nf_datasets_mnist, only: label_digits, load_mnist
+   use nf_layer, only: layer
+   use nf_layer_constructors, only: &
+      conv2d, dense, flatten, input, maxpool2d, reshape
+   use nf_loss, only: mse, quadratic, binary_entropy
+   use nf_network, only: network
+   use nf_optimizers, only: sgd, rmsprop, adam, adagrad
+   use nf_activation, only: activation_function, elu, exponential,  &
+      gaussian, linear, relu, leaky_relu,     &
+      sigmoid, softmax, softplus, step, tanhf, &
+      celu
 end module nf
